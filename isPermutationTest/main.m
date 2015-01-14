c16600192e585a784f4fee6d30d919a3f9f0ea5f
jen2 <jenniferasipila@Jennifers-MacBook-Air-3.local>
2015-01-14 16:54:35 -0500

    isPermutation.v1
    First try on the code

--- isPermutationTest/main.m
+++ isPermutationTest/main.m
@@ -15,3 +15,63 @@ int main(int argc, const char * argv[]) {
     }
     return 0;
 }
+
+
+NSString *str1;
+
+NSString *str2;
+
+
+NSString* str1 = @"ABCDE";
+
+NSString* str2 = @"AEIOU";
+
+
+
+bool isPermutation(NSString* str1, NSString* str2)
+
+{
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    
+    
+    if (len1 != len2)
+    {
+        
+        return false;
+        
+    }
+    
+    
+    bool*Matched = malloc (len2* sizeof(bool));
+    
+    for (int i = 0; i < len1; i++)
+    {
+        
+        for (int j = 0; j < len2; j ++)
+        {
+            
+            if (Matched [j] == true )
+                
+                Matched[j] = true;
+            
+            continue;
+            
+            if (str1[i] == str2[j])
+            {
+                break;
+                
+                
+            }
+            if (j == len2)
+                return false;
+        }
+        
+        free(Matched);
+        return true;
+    }
+    
+    
+    
+}
+
