de2eb53b0249b1ba325baf3d1159044fd0d17dc9
jen2 <jenniferasipila@Jennifers-MacBook-Air-3.local>
2015-01-14 16:51:34 -0500

    Initial Commit

---
+++ isPermutationTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  isPermutationTest
+//
+//  Created by Jennifer A Sipila on 1/14/15.
+//  Copyright (c) 2015 Jennifer A Sipila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
